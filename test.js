28596b769d4a3a93e26264a5e93c40cb7ea5de6f
ralmhana <ralmhana@uchicago.edu>
2025-07-10 15:09:59 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,24 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
 
+// Test configuration
 export const options = {
-  iterations: 10,
+  thresholds: {
+    // Assert that 99% of requests finish within 3000ms.
+    http_req_duration: ["p(99) < 100"],
+  },
+  // Ramp the number of virtual users up and down
+  stages: [
+    { duration: "30s", target: 15 },
+    { duration: "1m", target: 15 },
+    { duration: "20s", target: 0 },
+  ],
 };
 
-// The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
+// Simulated user behavior
 export default function () {
-  // Make a GET request to the target URL
-  http.get('https://quickpizza.grafana.com');
-
-  // Sleep for 1 second to simulate real-world usage
+  let res = http.get("https://quickpizza.grafana.com");
+  // Validate response status
+  check(res, { "status was 200": (r) => r.status == 200 });
   sleep(1);
 }
